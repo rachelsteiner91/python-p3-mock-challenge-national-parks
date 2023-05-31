e8b9eb7fa9758e80c2647ad1a40a7f6a290b424c
rachelsteiner91 <127536637+rachelsteiner91@users.noreply.github.com>
2023-05-31 11:27:40 -0400

    attempt 1 with many failed tests

--- lib/classes/national_park.py
+++ lib/classes/national_park.py
@@ -5,13 +5,34 @@ class NationalPark:
         self._trips = []
         self._visitors = []
         
+
+       
+        
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance (name, str) and not hasattr(self, 'name'):
+            self._name = name
+        else:
+            raise Exception("Name should not change after NationaPark is created")
+
+
     def trips(self, new_trip=None):
         from classes.trip import Trip
-        pass
+        if isinstance(new_trip, Trip):
+            self._trips.append(new_trip)
+        else:
+            raise Exception("must be of type Trip")
     
     def visitors(self, new_visitor=None):
         from classes.visitor import Visitor
-        pass
+        if isinstance(new_visitor, Visitor):
+            self.visitors.append(new_visitor)
+        else:
+            raise Exception("must of type Visitor")
     
     def total_visits(self):
         pass
--- lib/classes/visitor.py
+++ lib/classes/visitor.py
@@ -1,12 +1,33 @@
 class Visitor:
+    
 
     def __init__(self, name):
         self.name = name
+        self._trips = []
+        self._national_park = []
+        # Visitor.all.append(self)
+
+
+    @property
+    def name(self):
+        return self.__name 
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 15 and not hasattr(self, 'name'):
+            self._name = name
+        else:            
+            raise Exception("Name must be between 1 and 15 characters inclusive")
         
     def trips(self, new_trip=None):
         from classes.trip import Trip
-        pass
+        if isinstance(new_trip, Trip):
+            self._trips.append(new_trip)
+        else:
+            raise Exception("trips must be type Trip")
     
     def national_parks(self, new_national_park=None):
         from classes.national_park import NationalPark
-        pass
+        if isinstance(new_national_park, NationalPark):
+            self._national_park.append(new_national_park)
+        else:
+            raise Exception("national park must be type National Park")
